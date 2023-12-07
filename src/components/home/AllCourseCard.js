1f7e0c239d87d754748f863ac27e2680c7d0151d
dhananjoy <deydhananjoy5@gmail.com>
2023-12-07 10:32:46 +0530

    quick css fix to remove gap on right of all-course page

--- src/components/home/AllCourseCard.js
+++ src/components/home/AllCourseCard.js
@@ -7,7 +7,7 @@ const AllCourseCard = ({ filteredCourses }) => {
     }
   
     return (
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-x-14 gap-y-14">
         {filteredCourses.map((course) => (
           <CourseCard course={course} key={course.id} />
         ))}
